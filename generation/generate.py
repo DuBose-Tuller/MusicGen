b23f133f5eb07d560039f2057e6ec87dd64a947a
DuBose <dutuller@davidson.edu>
2024-10-21 20:12:11 -0400

    Switch to using audiocraft_fork

--- generation/generate.py
+++ generation/generate.py
@@ -1,13 +1,22 @@
+import sys
+import os
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
 import torchaudio
-from audiocraft.models import MusicGen
-from audiocraft.data.audio import audio_write
+from audiocraft_fork.audiocraft.models import MusicGen
+from audiocraft_fork.audiocraft.data.audio import audio_write
+# from audiocraft.models import MusicGen
+# from audiocraft.data.audio import audio_write
 
 model = MusicGen.get_pretrained('facebook/musicgen-melody')
-model.set_generation_params(duration=30)  # duration in seconds
+model.set_generation_params(duration=17)  # duration in seconds
+
+
 
 melody, sr = torchaudio.load('../data/synthetic-high/raw/high_tone_1062_phase_1.456.wav')
 # generates using the melody from the given audio
-wav, tokens = model.generate_continuation(melody[None], sr, return_tokens=True)
+wav, tokens = model.generate_continuation(melody[None], sr, return_tokens=True, progress=True)
+print(f"Final generated tokens shape: {tokens.shape}")
 
 for idx, one_wav in enumerate(wav):
     # Will save under {idx}.wav, with loudness normalization at -14 db LUFS.
