eb80198df2b5f842ff3e6f880841de9b43798768
DuBose <dutuller@davidson.edu>
2024-11-09 10:40:35 -0500

    don't overwrite existing data

--- generation/generate.py
+++ generation/generate.py
@@ -9,15 +9,15 @@ from audiocraft_fork.audiocraft.data.audio import audio_write
 # from audiocraft.data.audio import audio_write
 
 model = MusicGen.get_pretrained('facebook/musicgen-melody')
-model.set_generation_params(duration=17)  # duration in seconds
+model.set_generation_params(duration=15)  # duration in seconds
 
+path = '../data/synthetic-high/s5-t5/high_tone_1062_phase_1.456_0.wav'
 
-
-melody, sr = torchaudio.load('../data/synthetic-high/raw/high_tone_1062_phase_1.456.wav')
+melody, sr = torchaudio.load(path)
 # generates using the melody from the given audio
 wav, tokens = model.generate_continuation(melody[None], sr, return_tokens=True, progress=True)
 print(f"Final generated tokens shape: {tokens.shape}")
 
 for idx, one_wav in enumerate(wav):
     # Will save under {idx}.wav, with loudness normalization at -14 db LUFS.
-    audio_write(f'{idx}', one_wav.cpu(), model.sample_rate, strategy="loudness", loudness_compressor=True)
+    audio_write(path.split('/')[-1], one_wav.cpu(), model.sample_rate, strategy="loudness", loudness_compressor=True)
